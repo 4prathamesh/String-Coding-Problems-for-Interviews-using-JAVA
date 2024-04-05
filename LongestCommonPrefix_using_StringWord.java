0d0b54fdd8a190a84d284a112bd3c48569b569c4
4prathamesh <wprathamesh2@gmail.com>
2024-04-05 17:49:19 +0530

    add project

---
+++ LongestCommonPrefix_using_StringWord.java
@@ -0,0 +1,17 @@
+// Problem Statement: Given a set of strings, find the longest common prefix.
+// Examples:
+// Input: {“geeksforgeeks”, “geeks”, “geek”, “geezer”}
+// Output: “gee”
+// Input: {“apple”, “ape”, “april”}
+// Output: “ap”
+import java.util.*;
+public class LongestCommonPrefix_using_StringWord
+{
+    public static void main(String x[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the String");
+        String str=sc.nextLine();
+
+    }
+}
---
+++ Reverse_Words_of_String.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class Reverse_Words_of_String
+{
+    public static void main(String x[]) throws IOException
+    {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        //Scanner sc= new Scanner(System.in);
+        System.out.println("enter the String");
+        String str=br.readLine();
+
+        String str1[]=str.split(" ");
+
+       // System.out.println(str1[0]);
+        for(int i=0;i<str1.length/2;i++)
+        {
+            String temp=str1[i];
+            str1[i]=str1[str1.length-i-1];
+            str1[str1.length-i-1]=temp;
+        }
+        for(int i=0;i<str1.length;i++)
+        {
+            System.out.print(str1[i]+"\t");
+        }
+    }
+}
