fe6a8a0ada079169cff40401e6965adb9ac9dd8b
4prathamesh <wprathamesh2@gmail.com>
2024-04-07 00:13:37 +0530

    String 2 problem
    Longest Common Prefix using Sorting
    Problem Statement: Given a set of strings, find the longest common prefix.
    Examples:
    
    Input: {“geeksforgeeks”, “geeks”, “geek”, “geezer”}
    Output: “gee”
    
    Input: {“apple”, “ape”, “april”}
    Output: “ap”

--- LongestCommonPrefix_using_StringWord.java
+++ LongestCommonPrefix_using_StringWord.java
@@ -1,17 +1,36 @@
-// Problem Statement: Given a set of strings, find the longest common prefix.
-// Examples:
-// Input: {“geeksforgeeks”, “geeks”, “geek”, “geezer”}
-// Output: “gee”
-// Input: {“apple”, “ape”, “april”}
-// Output: “ap”
+
 import java.util.*;
 public class LongestCommonPrefix_using_StringWord
 {
     public static void main(String x[])
     {
         Scanner sc= new Scanner(System.in);
-        System.out.println("enter the String");
-        String str=sc.nextLine();
+        System.out.println("enter the size");
+        int size=sc.nextInt();
+        sc.nextLine();
+        String str[]= new String[size];
+        System.out.println("enter the words");
+        for(int i=0;i<size;i++)
+        {
+            str[i]=sc.nextLine();
+        }
+
+        int j = 0;
+        boolean flag = true; // Set initial flag to true
+        while (flag && j < str[0].length()) { // Iterate until flag becomes false or j exceeds length of any word
+            char ch = str[0].charAt(j); // Get character at index j from the first word
+            for (int i = 1; i < size; i++) {
+                // If character at index j in any word doesn't match, set flag to false and break the loop
+                if (j >= str[i].length() || str[i].charAt(j) != ch) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.print(ch); // Print common character at index j
+                j++;
+            }
+        }
 
     }
 }
