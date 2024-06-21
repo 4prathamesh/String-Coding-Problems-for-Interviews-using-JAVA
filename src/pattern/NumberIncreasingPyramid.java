6ae52870dda672d2849a9c8867c34b4f28df23ef
4prathamesh <wprathamesh2@gmail.com>
2024-06-21 23:34:11 +0530

    NumberIncreasingPyramid

---
+++ src/pattern/NumberIncreasingPyramid.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class NumberIncreasingPyramid {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
