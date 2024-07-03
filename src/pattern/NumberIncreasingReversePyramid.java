b0fcec98fe53026d472e3a2104b2cbbee1276830
4prathamesh <wprathamesh2@gmail.com>
2024-07-03 23:22:00 +0530

    Pattern program

---
+++ src/pattern/NumberIncreasingReversePyramid.java
@@ -0,0 +1,24 @@
+//1234
+//123
+//12
+//1
+package pattern;
+public class NumberIncreasingReversePyramid {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++)
+			{
+				if(j<=5-i)
+				{
+					System.out.printf("%d",j);				
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
